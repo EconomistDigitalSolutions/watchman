cd4ec69679cc6e1f0ea3dca2d49491f448e44555
buddhamagnet <buddhamagnet@gmail.com>
2015-09-07 15:09:33 +0100

    add ability to output to a writer or file and documen functions

--- journal/journal.go
+++ journal/journal.go
@@ -1,6 +1,8 @@
 package journal
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"time"
@@ -20,22 +22,42 @@ func init() {
 	stdlog.SetOutput(kitlog.NewStdlibAdapter(logger))
 }
 
+// SetLogger allows user to output log data to a writer.
+func SetLogger(w io.Writer) {
+	logger = kitlog.NewJSONLogger(w)
+	stdlog.SetOutput(kitlog.NewStdlibAdapter(logger))
+}
+
+// SetLogFile allows the user to output log data to a file.
+func SetLogFile(w io.Writer, file string) {
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		LogError(fmt.Sprintf("error opening log file: %v", err))
+	}
+	SetLogger(f)
+}
+
+// LogRequest logs details of an HTTP request.
 func LogRequest(r *http.Request) {
 	logger.Log("channel", "request", "service", Service, "method", r.Method, "url", r.URL.String(), "headers", r.Header, "ts", time.Now())
 }
 
+// LogChannel logs data to a log channel.
 func LogChannel(channel string, message ...interface{}) {
 	logger.Log("channel", channel, "service", Service, "message", message, "ts", time.Now())
 }
 
+// LogError logs error data to the error channel
 func LogError(message string) {
-	LogChannel("channel", message)
+	LogChannel("error", message)
 }
 
+// LogInfo logs informational data.
 func LogInfo(message string) {
 	LogChannel("information", message)
 }
 
+// LogWorker logs information around queue worker operations.
 func LogWorker(message ...interface{}) {
 	LogChannel("worker", message)
 }
